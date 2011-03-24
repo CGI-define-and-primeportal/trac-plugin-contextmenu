2b4701de8f70c42f1ba9f423790058b0b62b35a5
Pontus Enmark <pontus.enmark@cgi.com>
2011-03-24 21:40:33 +0000

    Account for scrolltop when in a scrolling div. See #552, fixes #791
    git-svn-id: file:///mnt/ramdisk/d4-svn/trunk@2808 484df14f-75d2-4b3f-8ba0-490e05069a01

--- contextmenu/htdocs/contextmenu.js
+++ contextmenu/htdocs/contextmenu.js
@@ -2,12 +2,21 @@ jQuery(function($){
   $('div.context-menu').live('click', function(e) {
     var holder = $(this)
     var menu = $('div:first', holder)
+    function findScrollTop(e) {
+      var s = e.scrollTop()
+      if (s) return s
+      if (e.parent().length)
+        return findScrollTop(e.parent())
+      return 0
+    }
     if (menu.is(':visible')) {
       menu.fadeOut('fast')
     } else {
       $('div.context-menu div.ctx-foldable').hide()
       var row = holder.closest('tr')
-      var top = row.position().top + row.outerHeight() - 1
+      // Add scrolltop if we're in a scrolling div
+      var scrollTop = findScrollTop($(this))
+      var top = row.position().top + row.outerHeight() - 1 + scrollTop
       var left = holder.position().left
       var bgColor = row.css('background-color')
       menu.css({top: top,
