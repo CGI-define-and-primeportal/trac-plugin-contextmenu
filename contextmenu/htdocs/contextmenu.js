2751db25cd7e3f7ca2aad11da8295ded1c1101ce
Sudha Selvaraj <sudha.selvaraj@cgi.com>
2012-11-12 12:35:04 +0000

    Merge [8863,8870 ] to 4.5.x for #2488
    git-svn-id: file:///mnt/ramdisk/d4-svn/branches/releases/4.5.x@8871 484df14f-75d2-4b3f-8ba0-490e05069a01

--- contextmenu/htdocs/contextmenu.js
+++ contextmenu/htdocs/contextmenu.js
@@ -2,21 +2,12 @@ jQuery(function($){
   $('div.context-menu').live('click blur', function(e) {
     var holder = $(this)
     var menu = $('.ctx-foldable', holder)
-    function findScrollTop(e) {
-      var s = e.scrollTop()
-      if (s) return s
-      if (e.parent().length)
-        return findScrollTop(e.parent())
-      return 0
-    }
     if (e.type == 'click') {
       if (menu.is(':visible')) {
         menu.fadeOut('fast')
       } else {
         var row = holder.closest('tr')
-        // Add scrolltop if we're in a scrolling div
-        var scrollTop = findScrollTop($(this))
-        var top = row.position().top + row.outerHeight() - 1 + scrollTop
+        var top = row.position().top + row.outerHeight() - 1
         var left = holder.position().left
         var bgColor = row.css('background-color')
         menu.css({top: top, width:'auto',
