bf19213a89d92dde3b5d084493f0db66338629bf
Ronny Östman <ronny.ostman@cgi.com>
2014-08-13 11:12:46 +0000

    Reverse merged changes made to #4623 regarding using an repository alias.
    git-svn-id: file:///mnt/ramdisk/d4-svn/trunk@24377 484df14f-75d2-4b3f-8ba0-490e05069a01

--- contextmenu/contextmenu.py
+++ contextmenu/contextmenu.py
@@ -81,7 +81,6 @@ class SubversionLink(Component):
     implements(ISourceBrowserContextMenuProvider)
 
     svn_base_url = Option('svn', 'repository_url')
-    default_repo_alias = Option('repositories', '.alias')
 
     # IContextMenuProvider methods
     def get_order(self, req):
@@ -115,12 +114,7 @@ class SubversionLink(Component):
 
     def get_subversion_href(self, data, path):
         href = self.svn_base_url.rstrip('/')
-        # The second part of this condition excludes the default svn repo alias 
-        # from the Subversion URL. This will no longer be needed once we support
-        # multiple named svn repositories and should be removed as part of #4755.
-        # TODO: Remove "and data['reponame'] != self.default_repo_alias" as part
-        # of #4755.
-        if data['reponame'] and data['reponame'] != self.default_repo_alias:
+        if data['reponame']:
             href += '/' + data['reponame']
         if path != '/':
             href += '/' + path
